5c53ae69c72cb47c0fbbc2280b5735f144bc0c84
刘雪彦 <liuxy@yunhuni.com>
2016-11-21 14:39:24 +0800

    modify: fabfiles

--- fabfiles/ipsc.py
+++ fabfiles/ipsc.py
@@ -10,28 +10,24 @@ IPSC_DIR = '/home/hesong/ipsc'
 IPSC_SOL_DIR = '/home/hesong/service_logic'
 
 
-@roles('ipsc')
 @task
 def start():
     with cd(IPSC_DIR):
         run('./fight -s')
 
 
-@roles('ipsc')
 @task
 def stop():
     with cd(IPSC_DIR):
         run('./fight -q')
 
 
-@roles('ipsc')
 @task
 def reload():
     with cd(IPSC_DIR):
         run('./reload')
 
 
-@roles('ipsc')
 @task
 def restart():
     with cd(IPSC_DIR):
@@ -39,20 +35,17 @@ def restart():
         execute(start)
 
 
-@roles('ipsc')
 @task
 def upload():
     put('../ipsc_solution', IPSC_SOL_DIR)
 
 
-@roles('ipsc')
 @task
 def deploy():
     execute(upload)
     execute(reload)
 
 
-@roles('ipsc')
 @task
 def pull(branch='master'):
     repo_dir = '/root/yunhuni-peer-comm-cti-flow'
